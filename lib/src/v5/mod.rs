14aee23c7e686fd8eb94a261e2a6361e7e544a0d
Markus Stange <mstange@themasta.com>
2020-05-14 18:55:26 -0400

    Checkpoint.

--- lib/src/v5/mod.rs
+++ lib/src/v5/mod.rs
@@ -86,7 +86,7 @@ mod response_json {
         pub frame: u32,
 
         #[serde(serialize_with = "as_hex_string")]
-        pub module_offset: u64,
+        pub module_offset: u32,
 
         pub module: String,
 
@@ -119,24 +119,92 @@ pub async fn get_api_response(
     let request: request_json::Request = serde_json::from_str(request_json_data)?;
     let mut requested_addresses: HashMap<Lib, Vec<u32>> = HashMap::new();
     for job in request.jobs() {
+        let mut requested_addresses_by_module_index: HashMap<u32, Vec<u32>> = HashMap::new();
         for stack in &job.stacks {
             for frame in &stack.0 {
-                let lib = job.memory_map.get(frame.module_index as usize).ok_or(
-                    GetSymbolsError::ParseRequestErrorContents(
-                        "Stack frame module index indexes beyond the memoryMap",
-                    ),
-                )?;
-                requested_addresses
-                    .entry((*lib).clone())
+                requested_addresses_by_module_index
+                    .entry(frame.module_index)
                     .or_insert_with(Vec::new)
                     .push(frame.address);
             }
         }
+        for (module_index, addresses) in requested_addresses_by_module_index {
+            let lib = job.memory_map.get(module_index as usize).ok_or(
+                GetSymbolsError::ParseRequestErrorContents(
+                    "Stack frame module index beyond the memoryMap",
+                ),
+            )?;
+            requested_addresses
+                .entry((*lib).clone())
+                .or_insert_with(Vec::new)
+                .extend(addresses);
+        }
     }
+    let mut symbolicated_addresses: HashMap<Lib, Option<HashMap<u32, AddressResult>>> =
+        HashMap::new();
     for (lib, addresses) in requested_addresses.into_iter() {
-        let address_results = get_address_results(&lib, addresses, helper).await;
+        let address_results = get_address_results(&lib, addresses, helper).await.ok();
+        symbolicated_addresses.insert(lib, address_results);
     }
-    Ok(String::from(""))
+
+    let response = response_json::Response {
+        results: request
+            .jobs()
+            .map(|job| {
+                let mut found_modules = HashMap::new();
+                let mut symbolicated_addresses_by_module_index = HashMap::new();
+                for (module_index, lib) in job.memory_map.iter().enumerate() {
+                    if let Some(symbols) = symbolicated_addresses.get(lib) {
+                        found_modules.insert(
+                            format!("{}/{}", lib.debug_name, lib.breakpad_id),
+                            symbols.is_some(),
+                        );
+                        if let Some(symbols) = symbols {
+                            symbolicated_addresses_by_module_index
+                                .insert(module_index as u32, symbols);
+                        }
+                    }
+                }
+
+                response_json::Result {
+                    stacks: job
+                        .stacks
+                        .iter()
+                        .map(|frames| {
+                            response_json::Stack(
+                                frames
+                                    .0
+                                    .iter()
+                                    .enumerate()
+                                    .map(|(frame_index, frame)| response_json::StackFrame {
+                                        frame: frame_index as u32,
+                                        module_offset: frame.address,
+                                        module: job.memory_map[frame.module_index as usize]
+                                            .debug_name
+                                            .clone(),
+                                        symbol: symbolicated_addresses_by_module_index
+                                            .get(&frame.module_index)
+                                            .map(|symbol_map| {
+                                                let address_result =
+                                                    symbol_map.get(&frame.address).unwrap();
+                                                response_json::Symbol {
+                                                    function: address_result.symbol_name.clone(),
+                                                    function_offset: frame.address
+                                                        - address_result.symbol_address,
+                                                }
+                                            }),
+                                    })
+                                    .collect(),
+                            )
+                        })
+                        .collect(),
+                    found_modules,
+                }
+            })
+            .collect(),
+    };
+
+    Ok(serde_json::to_string_pretty(&response)?)
 }
 
 struct AddressResult {
