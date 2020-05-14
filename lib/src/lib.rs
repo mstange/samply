bf3e29b078f7f116fea928a42226ac06da1f5421
Markus Stange <mstange@themasta.com>
2020-05-13 20:55:09 -0400

    Remove non-async get_compact_symbol_table.

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -17,34 +17,6 @@ use std::pin::Pin;
 pub use crate::compact_symbol_table::CompactSymbolTable;
 pub use crate::error::{GetSymbolsError, Result};
 
-pub fn get_compact_symbol_table(
-    binary_data: &[u8],
-    debug_data: &[u8],
-    breakpad_id: &str,
-) -> Result<CompactSymbolTable> {
-    let mut reader = Cursor::new(binary_data);
-    match goblin::peek(&mut reader)? {
-        Hint::Elf(_) => elf::get_compact_symbol_table(binary_data, breakpad_id),
-        Hint::Mach(_) => macho::get_compact_symbol_table(binary_data, breakpad_id),
-        Hint::MachFat(_) => {
-            let mut errors = vec![];
-            let multi_arch = mach::MultiArch::new(binary_data)?;
-            for fat_arch in multi_arch.iter_arches().filter_map(std::result::Result::ok) {
-                let arch_slice = fat_arch.slice(binary_data);
-                match macho::get_compact_symbol_table(arch_slice, breakpad_id) {
-                    Ok(table) => return Ok(table),
-                    Err(err) => errors.push(err),
-                }
-            }
-            Err(GetSymbolsError::NoMatchMultiArch(errors))
-        }
-        Hint::PE => pdb::get_compact_symbol_table(debug_data, breakpad_id),
-        _ => Err(GetSymbolsError::InvalidInputError(
-            "goblin::peek fails to read",
-        )),
-    }
-}
-
 pub trait OwnedFileData {
     fn get_data(&self) -> &[u8];
 }
@@ -221,19 +193,4 @@ fn pe_signature_to_uuid(identifier: &[u8; 16]) -> uuid::Uuid {
     data[6..8].reverse(); // uuid field 3
 
     uuid::Uuid::from_bytes(data)
-}
-
-#[cfg(test)]
-mod test {
-
-    #[test]
-    fn it_works() {
-        let mut file = std::fs::File::open(
-            "/Users/mstange/code/profiler-get-symbols/fixtures/win64-ci/softokn3.dll",
-        )
-        .unwrap();
-        let mut buffer = Vec::new();
-        use std::io::Read;
-        file.read_to_end(&mut buffer).unwrap();
-    }
 }
