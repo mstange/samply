a9eff55407685d26d0f2a2938fe0f6f3a68e5825
Jeff Muizelaar <jrmuizel@gmail.com>
2021-10-06 15:10:28 -0400

    Support filtering in dump

--- etw-reader/examples/dump.rs
+++ etw-reader/examples/dump.rs
@@ -8,6 +8,7 @@ fn main() {
 
     let mut schema_locator = SchemaLocator::new();
     etw_reader::add_custom_schemas(&mut schema_locator);
+    let pattern = std::env::args().nth(2);
     open_trace(Path::new(&std::env::args().nth(1).unwrap()), 
 |e| { 
     //dbg!(e.EventHeader.TimeStamp);
@@ -15,7 +16,11 @@ fn main() {
 
     let s = schema_locator.event_schema(e);
     if let Ok(s) = s {
-
+        if let Some(pattern) = &pattern {
+            if !s.name().contains(pattern) {
+                return;
+            }
+        }
         println!("{:?} {} {}-{} {} {}", e.EventHeader.ProviderId, s.name(),  e.EventHeader.EventDescriptor.Opcode, e.EventHeader.EventDescriptor.Id, s.property_count(), e.EventHeader.TimeStamp);
 
         let mut parser = Parser::create(&s);
@@ -25,8 +30,9 @@ fn main() {
             print_property(&mut parser, &property);
         }
     } else {
-        println!("unknown event {:x?}", e.EventHeader.ProviderId);
-
+        if pattern.is_none() {
+            println!("unknown event {:x?}:{}", e.EventHeader.ProviderId, e.EventHeader.EventDescriptor.Opcode);
+        }
     }
 
 
