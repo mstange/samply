8e8aead52d70073d9ae79edbb99837820ea7f114
Jeff Muizelaar <jrmuizel@gmail.com>
2021-10-06 15:46:09 -0400

    Add the names for some known GUIDs

--- etw-reader/examples/event-types.rs
+++ etw-reader/examples/event-types.rs
@@ -1,10 +1,4 @@
-use etw_reader::{
-    open_trace,
-    parser::{Parser, TryParse},
-    print_property,
-    schema::{EventSchema, SchemaLocator},
-    tdh_types::{Property, TdhInType},
-};
+use etw_reader::{Guid, open_trace, parser::{Parser, TryParse}, print_property, schema::{EventSchema, SchemaLocator}, tdh_types::{Property, TdhInType}};
 use std::{cmp::Reverse, collections::HashMap, path::Path};
 
 fn main() {
@@ -20,7 +14,16 @@ fn main() {
                 event_counts.insert(s.name().to_owned(), 1);
             }
         } else {
-            let provider = format!("{:?}/{}", e.EventHeader.ProviderId, e.EventHeader.EventDescriptor.Opcode);
+            let provider_name = 
+            match e.EventHeader.ProviderId {
+                Guid{ data1: 0x9B79EE91, data2: 0xB5FD, data3: 0x41C0, data4: [0xA2, 0x43, 0x42, 0x48, 0xE2, 0x66, 0xE9, 0xD0]} => "SysConfig ",
+                Guid{ data1: 0xB3E675D7, data2: 0x2554, data3: 0x4F18, data4: [0x83, 0x0B, 0x27, 0x62, 0x73, 0x25, 0x60, 0xDE]} => "KernelTraceControl ",
+                Guid{ data1: 0xED54DFF8, data2: 0xC409, data3: 0x4CF6, data4: [0xBF, 0x83, 0x05, 0xE1, 0xE6, 0x1A, 0x09, 0xC4]} => "WinSat ",
+
+                _ => ""
+            };
+
+            let provider = format!("{}{:?}/{}", provider_name,  e.EventHeader.ProviderId, e.EventHeader.EventDescriptor.Opcode);
             *event_counts.entry(provider).or_insert(0) += 1;
         }
     });
