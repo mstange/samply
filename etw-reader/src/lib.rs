a02bb47bb161ba4af77098c72217698905c429a6
Jeff Muizelaar <jrmuizel@gmail.com>
2021-10-25 14:26:25 -0400

    Improve printing of property errors and fix u16

--- etw-reader/src/lib.rs
+++ etw-reader/src/lib.rs
@@ -11,7 +11,7 @@ extern crate bitflags;
 extern crate num_derive;
 
 
-use crate::{bindings::Windows::Win32::Foundation::PWSTR, parser::{Parser, TryParse}, schema::{EventSchema, SchemaLocator}, tdh_types::TdhInType};
+use crate::{bindings::Windows::Win32::Foundation::PWSTR, parser::{Parser, ParserError, TryParse}, schema::{EventSchema, SchemaLocator}, tdh_types::TdhInType};
 use etw_types::EventRecord;
 use schema::TypedEvent;
 use tdh_types::Property;
@@ -105,18 +105,25 @@ pub fn print_property(parser: &mut Parser, property: &Property) {
             println!("{}", map_info.map[&value]);
         }
     } else {
-        match property.in_type() {
-            TdhInType::InTypeUnicodeString => println!("{:?}", TryParse::<String>::try_parse(parser, &property.name)),
-            TdhInType::InTypeAnsiString => println!("{:?}", TryParse::<String>::try_parse(parser, &property.name)),
-            TdhInType::InTypeUInt32 => println!("{:?}", TryParse::<u32>::try_parse(parser, &property.name)),
-            TdhInType::InTypeUInt16 => println!("{:?}", TryParse::<u32>::try_parse(parser, &property.name)),
-            TdhInType::InTypeUInt8 => println!("{:?}", TryParse::<u8>::try_parse(parser, &property.name)),
-            TdhInType::InTypePointer => println!("{:?}", TryParse::<u64>::try_parse(parser, &property.name)),
-            TdhInType::InTypeInt64 => println!("{:?}", TryParse::<i64>::try_parse(parser, &property.name)),
-            TdhInType::InTypeUInt64 => println!("{:?}", TryParse::<u64>::try_parse(parser, &property.name)),
-            TdhInType::InTypeGuid => println!("{:?}", TryParse::<Guid>::try_parse(parser, &property.name)),
-            _ => println!("Unknown {:?}", property.in_type())
-        }
+        let value = match property.in_type() {
+            TdhInType::InTypeUnicodeString => TryParse::<String>::try_parse(parser, &property.name),
+            TdhInType::InTypeAnsiString => TryParse::<String>::try_parse(parser, &property.name),
+            TdhInType::InTypeUInt32 => TryParse::<u32>::try_parse(parser, &property.name).map(|x| x.to_string()),
+            TdhInType::InTypeUInt16 => TryParse::<u16>::try_parse(parser, &property.name).map(|x| x.to_string()),
+            TdhInType::InTypeUInt8 => TryParse::<u8>::try_parse(parser, &property.name).map(|x| x.to_string()),
+            TdhInType::InTypeInt64 => TryParse::<i64>::try_parse(parser, &property.name).map(|x| x.to_string()),
+            TdhInType::InTypeUInt64 => TryParse::<u64>::try_parse(parser, &property.name).map(|x| x.to_string()),
+            TdhInType::InTypePointer => TryParse::<u64>::try_parse(parser, &property.name).map(|x| x.to_string()),
+            TdhInType::InTypeGuid => TryParse::<Guid>::try_parse(parser, &property.name).map(|x| format!("{:?}", x)),
+            _ => Ok(format!("Unknown {:?}", property.in_type()))
+        };
+        let value = match value {
+            Ok(value) => value,
+            Err(ParserError::InvalidType) => format!("invalid type {:?}", property.in_type()),
+            Err(ParserError::LengthMismatch) => format!("Err(LengthMismatch) type: {:?}, buf: {}", property.in_type(), parser.buffer.len()),
+            Err(e) => format!("Err({:?}) type: {:?}", e, property.in_type())
+        };
+        println!("{}", value)
     }
 }
 
