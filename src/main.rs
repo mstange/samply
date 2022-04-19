a289a62f9c6221042326e1b1b1a05e81efd0a650
Markus Stange <mstange.moz@gmail.com>
2022-04-19 00:02:22 -0400

    Improve DebugId computation code.

--- src/main.rs
+++ src/main.rs
@@ -206,10 +206,8 @@ where
                         continue;
                     }
 
-                    let debug_id = build_id.map(|buildid| {
-                        let uuid = create_elf_id(buildid, little_endian);
-                        DebugId::from_uuid(uuid)
-                    });
+                    let debug_id =
+                        build_id.map(|buildid| DebugId::from_elf_build_id(buildid, little_endian));
 
                     let start_addr = e.address;
                     let end_addr = e.address + e.length;
@@ -856,69 +854,73 @@ where
     );
     unwinder.add_module(module);
 
-    get_elf_id(&file).map(DebugId::from_uuid)
+    debug_id_for_object(&file)
 }
 
-const UUID_SIZE: usize = 16;
-const PAGE_SIZE: usize = 4096;
-
-fn create_elf_id(identifier: &[u8], little_endian: bool) -> Uuid {
-    // Make sure that we have exactly UUID_SIZE bytes available
-    let mut data = [0u8; UUID_SIZE];
-    let len = std::cmp::min(identifier.len(), UUID_SIZE);
-    data[0..len].copy_from_slice(&identifier[0..len]);
-
-    if little_endian {
-        // The file ELF file targets a little endian architecture. Convert to
-        // network byte order (big endian) to match the Breakpad processor's
-        // expectations. For big endian object files, this is not needed.
-        data[0..4].reverse(); // uuid field 1
-        data[4..6].reverse(); // uuid field 2
-        data[6..8].reverse(); // uuid field 3
-    }
-
-    Uuid::from_bytes(data)
+pub trait DebugIdElfExt {
+    /// Creates a DebugId from the build ID of an ELF object file.
+    /// The `little_endian` argument
+    fn from_elf_build_id(build_id: &[u8], little_endian: bool) -> Self;
 }
 
-/// Tries to obtain the object identifier of an ELF object.
-///
-/// As opposed to Mach-O, ELF does not specify a unique ID for object files in
-/// its header. Compilers and linkers usually add either `SHT_NOTE` sections or
-/// `PT_NOTE` program header elements for this purpose. If one of these notes
-/// is present, ElfFile's build_id() method will find it.
-///
-/// If neither of the above are present, this function will hash the first page
-/// of the `.text` section (program code). This matches what the Breakpad
-/// processor does.
-///
-/// If all of the above fails, this function will return `None`.
-pub fn get_elf_id<'data: 'file, 'file>(elf_file: &'file impl Object<'data, 'file>) -> Option<Uuid> {
-    if let Some(identifier) = elf_file.build_id().ok()? {
-        return Some(create_elf_id(identifier, elf_file.is_little_endian()));
+impl DebugIdElfExt for DebugId {
+    fn from_elf_build_id(build_id: &[u8], little_endian: bool) -> Self {
+        // Make sure that we have exactly UUID_SIZE bytes available.
+        // ELF build IDs are usually 20 bytes, so this performs a lossy truncation.
+        const UUID_SIZE: usize = 16;
+        let mut data = [0u8; UUID_SIZE];
+        let len = std::cmp::min(build_id.len(), UUID_SIZE);
+        data[0..len].copy_from_slice(&build_id[0..len]);
+
+        // For compatibility with the Breakpad symbol processor, endian-swap the
+        // build ID bytes for little-endian object files. This simulates serializing
+        // the three UUID fields u32, u16, u16 as big-endian. (The remaining 8 bytes
+        // are just bytes, not fields.)
+        if little_endian {
+            data[0..4].reverse(); // uuid field 1
+            data[4..6].reverse(); // uuid field 2
+            data[6..8].reverse(); // uuid field 3
+        }
+
+        DebugId::from_uuid(Uuid::from_bytes(data))
+    }
+}
+
+/// Tries to obtain a DebugId for an ELF object. This uses the build ID, if available,
+/// and falls back to hashing the first page of the .text section otherwise.
+/// Returns None on failure.
+pub fn debug_id_for_object<'data: 'file, 'file>(
+    elf_file: &'file impl Object<'data, 'file>,
+) -> Option<DebugId> {
+    if let Ok(Some(identifier)) = elf_file.build_id() {
+        return Some(DebugId::from_elf_build_id(
+            identifier,
+            elf_file.is_little_endian(),
+        ));
     }
 
-    // We were not able to locate the build ID, so fall back to hashing the
-    // first page of the ".text" (program code) section. This algorithm XORs
-    // 16-byte chunks directly into a UUID buffer.
-    if let Some(section_data) = find_text_section(elf_file) {
+    // We were not able to locate the build ID, so fall back to creating a synthetic
+    // build id from a hash of the first page of the ".text" (program code) section.
+    // This algorithm XORs 16-byte chunks directly into a 16-byte buffer.
+    if let Some(section_data) = elf_file
+        .sections()
+        .find(|header| header.kind() == SectionKind::Text)
+        .and_then(|header| header.data().ok())
+    {
+        const UUID_SIZE: usize = 16;
+        const PAGE_SIZE: usize = 4096;
         let mut hash = [0; UUID_SIZE];
         for i in 0..std::cmp::min(section_data.len(), PAGE_SIZE) {
             hash[i % UUID_SIZE] ^= section_data[i];
         }
 
-        return Some(create_elf_id(&hash, elf_file.is_little_endian()));
+        return Some(DebugId::from_elf_build_id(
+            &hash,
+            elf_file.is_little_endian(),
+        ));
     }
 
     None
-}
-
-/// Returns a reference to the data of the the .text section in an ELF binary.
-fn find_text_section<'data: 'file, 'file>(
-    file: &'file impl Object<'data, 'file>,
-) -> Option<&'data [u8]> {
-    file.sections()
-        .find(|header| header.kind() == SectionKind::Text)
-        .and_then(|header| header.data().ok())
 }
 
 struct FileContents(memmap2::Mmap);
