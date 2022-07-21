5e48c50c1bdbeb56f8b6ea8f33809dc47199bd0a
Markus Stange <mstange.moz@gmail.com>
2022-07-20 22:07:59 -0400

    Streamline samply-api crate documentation.

--- samply-api/src/lib.rs
+++ samply-api/src/lib.rs
@@ -1,54 +1,15 @@
-//! This crate allows obtaining symbol information from binaries and compilation artifacts.
-//! It maps raw code addresses to symbol strings, and, if available, file name + line number
-//! information.
-//! The API was designed for the Firefox profiler.
+//! This crate implements a JSON API for profiler symbolication with the help of
+//! local symbol files. It exposes a single function `query_api`, and uses the
+//! `samply-symbols` crate for its implementation.
 //!
-//! The main entry point of this crate is the async `query_api` function, which accepts a
-//! JSON string with the query input. The JSON API matches the API of the [Mozilla
-//! symbolication server ("Tecken")](https://tecken.readthedocs.io/en/latest/symbolication.html).
-//! An alternative JSON-free API is available too, but it is not very ergonomic.
-//!
-//! # Design constraints
-//!
-//! This crate operates under the following design constraints:
-//!
-//!  - Must be usable from JavaScript / WebAssembly: The Firefox profiler runs this code in a
-//!    WebAssembly environment, invoked from a privileged piece of JavaScript code inside Firefox itself.
-//!    This setup allows us to download the profiler-get-symbols wasm bundle on demand, rather than shipping
-//!    it with Firefox, which would increase the Firefox download size for a piece of functionality
-//!    that the vast majority of Firefox users don't need.
-//!  - Performance: We want to be able to obtain symbol data from a fresh build of a locally compiled
-//!    Firefox instance as quickly as possible, without an expensive preprocessing step. The time between
-//!    "finished compilation" and "returned symbol data" should be minimized. This means that symbol
-//!    data needs to be obtained directly from the compilation artifacts rather than from, say, a
-//!    dSYM bundle or a Breakpad .sym file.
-//!  - Must scale to large inputs: This applies to both the size of the API request and the size of the
-//!    object files that need to be parsed: The Firefox profiler will supply anywhere between tens of
-//!    thousands and hundreds of thousands of different code addresses in a single symbolication request.
-//!    Firefox build artifacts such as libxul.so can be multiple gigabytes big, and contain around 300000
-//!    function symbols. We want to serve such requests within a few seconds or less.
-//!  - "Best effort" basis: If only limited symbol information is available, for example from system
-//!    libraries, we want to return whatever limited information we have.
-//!
-//! The WebAssembly requirement means that this crate cannot contain any direct file access.
-//! Instead, all file access is mediated through a `FileAndPathHelper` trait which has to be implemented
-//! by the caller. Furthermore, the API request does not carry any absolute file paths, so the resolution
-//! to absolute file paths needs to be done by the caller as well.
-//!
-//! # Supported formats and data
-//!
-//! This crate supports obtaining symbol data from PE binaries (Windows), PDB files (Windows),
-//! mach-o binaries (including fat binaries) (macOS & iOS), and ELF binaries (Linux, Android, etc.).
-//! For mach-o files it also supports finding debug information in external objects, by following
-//! OSO stabs entries.
-//! It supports gathering both basic symbol information (function name strings) as well as information
-//! based on debug data, i.e. inline callstacks where each frame has a function name, a file name,
-//! and a line number.
-//! For debug data we support both DWARF debug data (inside mach-o and ELF binaries) and PDB debug data.
+//! Just like the `samply-symbols` crate, this crate does not contain any direct
+//! file access. It is written in such a way that it can be compiled to
+//! WebAssembly, with all file access being mediated via a `FileAndPathHelper`
+//! trait.
 //!
 //! # Example
 //!
-//! ```
+//! ```rust
 //! use samply_api::samply_symbols::{
 //!     FileContents, FileAndPathHelper, FileAndPathHelperResult, OptionallySendFuture,
 //!     CandidatePathInfo, FileLocation
