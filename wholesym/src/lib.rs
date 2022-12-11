6606010d6690c166bc22f13f330edd02d7622875
Markus Stange <mstange.moz@gmail.com>
2022-12-11 15:52:47 -0500

    Expose FilePath from wholesym.

--- wholesym/src/lib.rs
+++ wholesym/src/lib.rs
@@ -11,6 +11,6 @@ pub use config::{LibraryInfo, SymbolManagerConfig};
 pub use samply_api::samply_symbols;
 pub use samply_api::samply_symbols::{
     AddressInfo, Error, ExternalFileAddressInFileRef, ExternalFileAddressRef, ExternalFileRef,
-    ExternalFileSymbolMap, FramesLookupResult, InlineStackFrame, SymbolInfo, SymbolMap,
+    ExternalFileSymbolMap, FilePath, FramesLookupResult, InlineStackFrame, SymbolInfo, SymbolMap,
 };
 pub use symbol_manager::SymbolManager;
