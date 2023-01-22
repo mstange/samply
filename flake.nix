994d4128f123ee6526efbf215e8b42d02685e12a
jakob <jak.gerhardt@gmail.com>
2023-01-22 11:35:32 +0100

    disable checks in flake.nix

--- flake.nix
+++ flake.nix
@@ -78,46 +78,6 @@
         checks = {
           # Build the crate as part of `nix flake check` for convenience
           inherit samply;
-
-          # Run clippy (and deny all warnings) on the crate source,
-          # again, resuing the dependency artifacts from above.
-          #
-          # Note that this is done as a separate derivation so that
-          # we can block the CI if there are issues here, but not
-          # prevent downstream consumers from building our crate by itself.
-          cargo-clippy = craneLib.cargoClippy {
-            inherit cargoArtifacts src buildInputs;
-            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-          };
-
-          cargo-doc = craneLib.cargoDoc {
-            inherit cargoArtifacts src buildInputs;
-          };
-
-          # Check formatting
-          cargo-fmt = craneLib.cargoFmt {
-            inherit src;
-          };
-
-          # Audit dependencies
-          cargo-audit = craneLib.cargoAudit {
-            inherit src advisory-db;
-          };
-
-          # Run tests with cargo-nextest
-          # Consider setting `doCheck = false` on `samply` if you do not want
-          # the tests to run twice
-          cargo-nextest = craneLib.cargoNextest {
-            inherit cargoArtifacts src buildInputs;
-            partitions = 1;
-            partitionType = "count";
-          };
-        } // lib.optionalAttrs (system == "x86_64-linux") {
-          # NB: cargo-tarpaulin only supports x86_64 systems
-          # Check code coverage (note: this will not upload coverage anywhere)
-          cargo-coverage = craneLib.cargoTarpaulin {
-            inherit cargoArtifacts src;
-          };
         };
 
         packages.default = samply;
