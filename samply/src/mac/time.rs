713f8c4429f745335008e847aba4f719972e9e5c
Markus Stange <mstange.moz@gmail.com>
2023-06-03 09:23:28 -0400

    For raw timestamps on macOS, use mach_absolute_time nanoseconds.

--- samply/src/mac/time.rs
+++ samply/src/mac/time.rs
@@ -1,8 +1,20 @@
+use mach::mach_time;
+use once_cell::sync::OnceCell;
+
+static NANOS_PER_TICK: OnceCell<mach_time::mach_timebase_info> = OnceCell::new();
+
 pub fn get_monotonic_timestamp() -> u64 {
-    let mut ts = libc::timespec {
-        tv_sec: 0,
-        tv_nsec: 0,
-    };
-    unsafe { libc::clock_gettime(libc::CLOCK_MONOTONIC, &mut ts) };
-    ts.tv_sec as u64 * 1_000_000_000 + ts.tv_nsec as u64
+    let nanos_per_tick = NANOS_PER_TICK.get_or_init(|| unsafe {
+        let mut info = mach_time::mach_timebase_info::default();
+        let errno = mach_time::mach_timebase_info(&mut info as *mut _);
+        if errno != 0 || info.denom == 0 {
+            info.numer = 1;
+            info.denom = 1;
+        };
+        info
+    });
+
+    let time = unsafe { mach_time::mach_absolute_time() };
+
+    time * nanos_per_tick.numer as u64 / nanos_per_tick.denom as u64
 }
