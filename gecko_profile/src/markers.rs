0a51d29936967815476b5a8f2139567e023542b9
Christopher Serr <christopher.serr@gmail.com>
2023-08-27 13:24:06 +0200

    Decouple `serde` from its `derive` crate
    By not activating the `derive` feature on `serde`, the compilation speed
    can be improved by a lot. This is because `serde` can then compile in
    parallel to `serde_derive`, allowing it to finish compilation possibly
    even before `serde_derive`, unblocking all the crates waiting for
    `serde` to start compiling much sooner.
    
    As it turns out the main deciding factor for how long the compile time of a
    project is, is primarly determined by the depth of dependencies rather
    than the width. In other words, a crate's compile times aren't affected
    by how many crates it depends on, but rather by the longest chain of
    dependencies that it needs to wait on. In many cases `serde` is part of
    that long chain, as it is part of a long chain if the `derive` feature
    is active:
    
    `proc-macro2` compile build script > `proc-macro2` run build script >
    `proc-macro2` > `quote` > `syn` > `serde_derive` > `serde` >
    `serde_json` (or any crate that depends on serde)
    
    By decoupling it from `serde_derive`, the chain is shortened and compile
    times get much better.
    
    Check this issue for a deeper elaboration:
    https://github.com/serde-rs/serde/issues/2584
    
    For `samply` I'm seeing a reduction from 5.43s to 3.70s when compiling
    `fxprof-processed-profile` in `release` mode.

--- gecko_profile/src/markers.rs
+++ gecko_profile/src/markers.rs
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use serde::Serialize;
+use serde_derive::Serialize;
 use serde_json::{json, Value};
 use std::time::Instant;
 
