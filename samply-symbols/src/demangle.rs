b057cba8c48d2660913d4ef9d858aecfc57275e4
Anton Sviridov <keynmol@gmail.com>
2024-03-07 09:57:28 +0000

    Add scala native demangling and tests

--- samply-symbols/src/demangle.rs
+++ samply-symbols/src/demangle.rs
@@ -14,6 +14,12 @@ pub fn demangle_any(name: &str) -> String {
         return msvc_demangler::demangle(name, flags).unwrap_or_else(|_| name.to_string());
     }
 
+    if name.starts_with("__S") {
+        if let Ok(symbol) = scala_native_demangle::demangle_with_defaults(&name[1..name.len()]) {
+            return symbol;
+        }
+    }
+
     if let Ok(demangled_symbol) = rustc_demangle::try_demangle(name) {
         return format!("{demangled_symbol:#}");
     }
@@ -36,4 +42,52 @@ pub fn demangle_any(name: &str) -> String {
     }
 
     name.to_owned()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::demangle::demangle_any;
+    #[test]
+    fn cpp_demangling() {
+        assert_eq!(
+            demangle_any("_ZNK8KxVectorI16KxfArcFileRecordjEixEj"),
+            "KxVector<KxfArcFileRecord, unsigned int>::operator[](unsigned int) const"
+        )
+    }
+
+    #[test]
+    fn mscvc_demangling() {
+        assert_eq!(
+            demangle_any("??_R3?$KxSet@V?$KxSpe@DI@@I@@8"),
+            "KxSet<KxSpe<char, unsigned int>, unsigned int>::`RTTI Class Hierarchy Descriptor'"
+        )
+    }
+
+    #[test]
+    fn rust_demangling() {
+        assert_eq!(
+            demangle_any(
+                "_RNvMsr_NtCs3ssYzQotkvD_3std4pathNtB5_7PathBuf3newCs15kBYyAo9fc_7mycrate"
+            ),
+            "<std::path::PathBuf>::new"
+        )
+    }
+
+    #[test]
+    fn ocaml_demangling() {
+        assert_eq!(demangle_any("camlA__b__c_1002"), "A.b.c_1002")
+    }
+
+    #[test]
+    fn scala_native_demangling() {
+        assert_eq!(
+            demangle_any("__SM17java.lang.IntegerD7compareiiiEo"),
+            "java.lang.Integer.compare(Int,Int): Int"
+        )
+    }
+
+    #[test]
+    fn no_demangling() {
+        assert_eq!(demangle_any("_!!!!!!!bla"), "!!!!!!!bla")
+    }
 }
