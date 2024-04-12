16a4ff67ee2da629dfaa33a33c98a27b3e1b9c70
Markus Stange <mstange.moz@gmail.com>
2024-04-12 13:20:45 -0400

    Enable dwo and dwp tests now that addr2line has updated.

--- wholesym/tests/integration_tests/main.rs
+++ wholesym/tests/integration_tests/main.rs
@@ -326,13 +326,6 @@ mod simple_example {
         .await;
     }
 
-    // This test is currently disabled.
-    // main.dwp has an empty unit index, so this test runs into https://github.com/gimli-rs/gimli/issues/705
-    // and fails.
-    // Most normal DWP files will work, but the one in this test does not.
-    // We can enable this test once we update to a gimli release which
-    // includes https://github.com/gimli-rs/gimli/pull/706 .
-    #[ignore]
     #[tokio::test]
     async fn run_test_with_dwp() {
         let dwp_obj_dir = fixtures_dir().join("other/simple-example/out/with-dwp");
@@ -345,8 +338,6 @@ mod simple_example {
         .await;
     }
 
-    // This test is disabled for the same reason as the previous test.
-    #[ignore]
     #[tokio::test]
     async fn run_test_dwp_debuglink() {
         let dwp_debuglink_obj_dir = fixtures_dir().join("other/simple-example/out/dwp-debuglink");
