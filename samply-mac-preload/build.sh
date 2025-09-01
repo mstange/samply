4b85d8b6521e459dcba19f6783e321dfd0672848
Markus Stange <mstange.moz@gmail.com>
2025-08-31 21:07:22 -0400

    Raise MACOSX_DEPLOYMENT_TARGET for samply-mac-preload to 10.12, the minimum version supported by Rust.

--- samply-mac-preload/build.sh
+++ samply-mac-preload/build.sh
@@ -1,5 +1,5 @@
 export SDKROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
-MACOSX_DEPLOYMENT_TARGET=10.7 cargo build --release --target=x86_64-apple-darwin
+MACOSX_DEPLOYMENT_TARGET=10.12 cargo build --release --target=x86_64-apple-darwin
 mv target/x86_64-apple-darwin/release/libsamply_mac_preload.dylib binaries/libsamply_mac_preload_x86_64.dylib
 MACOSX_DEPLOYMENT_TARGET=11.0 cargo build --release --target=aarch64-apple-darwin
 mv target/aarch64-apple-darwin/release/libsamply_mac_preload.dylib binaries/libsamply_mac_preload_arm64.dylib
