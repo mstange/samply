be07cb4d0193b1369e08b301c00e7566917d314d
Erik Nordin <nordzilla@nordzil.la>
2025-11-11 11:08:06 -0600

    Add samply_marker declarative macro
    This commit adds a declarative macro wrapper around the
    `SamplyMarker` implementation with the intent to be the
    primary way of emitting markers.

---
+++ samply-markers/src/macros/samply_marker.rs
@@ -0,0 +1,48 @@
+//! This module defines the [`samply_marker!`](crate::samply_marker) macro for emitting markers directly.
+
+/// Emits a marker at the current location in the code's execution.
+///
+/// * Providing only a `name` will emit an instant marker at the current time.
+/// * Providing an optional `start_time` will emit an interval marker from `start_time` to now.
+///
+/// # Examples
+///
+/// ```rust
+/// # use samply_markers::samply_marker;
+/// # use samply_markers::marker::SamplyTimestamp;
+/// # struct Request {}
+/// # impl Request { fn id(&self) -> u32 { 0 } }
+/// # fn receive_request() -> Request { Request {} }
+/// # fn query_database(id: u32) -> Vec<String> { vec![] }
+/// # fn serialize_json(data: &[String]) -> String { String::new() }
+/// let request = receive_request();
+///
+/// // Emit an instant marker.
+/// samply_marker!({
+///     name: format!("received request {}", request.id()),
+/// });
+///
+/// // Mark the start time for serializing the response.
+/// let start = SamplyTimestamp::now();
+///
+/// let data = query_database(request.id());
+/// let json = serialize_json(&data);
+///
+/// // Emit an interval marker.
+/// samply_marker!({
+///     name: "serialize response",
+///     start_time: start,
+/// });
+/// ```
+#[macro_export]
+macro_rules! samply_marker {
+    // Instant marker with only a name.
+    {{ name: $name:expr $(,)? }} => {{
+        $crate::marker::SamplyMarker::new($name).emit_instant();
+    }};
+
+    // Interval marker with a provided start time.
+    {{ name: $name:expr, start_time: $start_time:expr $(,)? }} => {{
+        $crate::marker::SamplyMarker::new($name).emit_interval($start_time);
+    }};
+}
