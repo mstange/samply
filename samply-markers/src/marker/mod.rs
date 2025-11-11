ea591b85f8101ecbcc0c5983dd4cfb9b7292400f
Erik Nordin <nordzilla@nordzil.la>
2025-11-11 08:35:16 -0600

    Add SamplyTimestamp with default provider
    This commit adds the `SamplyTimestamp` implementation
    with a default provider that always returns zero.

---
+++ samply-markers/src/marker/mod.rs
@@ -0,0 +1,5 @@
+//! This module contains the core types for instrumenting code with markers.
+
+mod timestamp;
+
+pub use timestamp::SamplyTimestamp;
--- samply-markers/src/provider/mod.rs
+++ samply-markers/src/provider/mod.rs
@@ -1,2 +1,24 @@
-#[cfg(all(feature = "enabled", target_family = "unix"))]
-pub mod unix;
+//! This module contains context-specific, uniform implementations for the internal APIs used by this crate.
+//!
+//! The default provider is designed to no-op when the code is not compiled for profiling.
+//!
+//! When [samply-markers](crate) is enabled, there are platform-specific provider implementations for Unix and Windows systems.
+//!
+//! * An internal provider must provide a type [`TimestampNowImpl`] that implements the [`TimestampNowProvider`] trait.
+//!
+//! [`TimestampNowProvider`]: crate::provider::TimestampNowProvider
+
+use crate::marker::SamplyTimestamp;
+
+pub use internal_provider::TimestampNowImpl;
+
+/// A trait implemented by context-specific providers to supply monotonic nanosecond timestamp.
+pub trait TimestampNowProvider {
+    /// Returns a monotonic timestamp in nanoseconds.
+    fn now() -> SamplyTimestamp;
+}
+
+#[cfg(not(feature = "enabled"))]
+pub mod r#default;
+#[cfg(not(feature = "enabled"))]
+pub use r#default as internal_provider;
