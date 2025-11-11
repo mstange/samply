68e61b9f9c5eebd345380fb89b77d1a7c0447ea0
Erik Nordin <nordzilla@nordzil.la>
2025-11-11 10:49:09 -0600

    Add SamplyMarker with default provider
    This commit adds the `SamplyMarker` implementation
    with a default provider that intentionally does not
    write the markers to a file.

--- samply-markers/src/marker/mod.rs
+++ samply-markers/src/marker/mod.rs
@@ -1,5 +1,7 @@
 //! This module contains the core types for instrumenting code with markers.
 
+mod samply_marker;
 mod timestamp;
 
+pub use samply_marker::SamplyMarker;
 pub use timestamp::SamplyTimestamp;
