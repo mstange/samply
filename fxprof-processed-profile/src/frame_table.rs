ade0693ebe9aa20c2ebbeb2522457fc6fdc7109e
Markus Stange <mstange.moz@gmail.com>
2026-07-30 19:55:44 -0400

    Store these two columns more compactly.

--- fxprof-processed-profile/src/frame_table.rs
+++ fxprof-processed-profile/src/frame_table.rs
@@ -3,7 +3,7 @@ use std::io::Write;
 use crate::category::{CategoryHandle, SubcategoryHandle, SubcategoryIndex};
 use crate::fast_hash_map::FastIndexSet;
 use crate::frame::FrameFlags;
-use crate::func_table::{FuncIndex, FuncKey, FuncTable};
+use crate::func_table::{FuncKey, FuncTable};
 use crate::global_lib_table::{GlobalLibIndex, UsedLibraryAddressesCollector};
 use crate::native_symbols::NativeSymbolIndex;
 use crate::resource_table::ResourceTable;
@@ -83,7 +83,7 @@ impl FrameInterner {
             let func_key = frame.func_key(&mut source_table, &mut resource_table);
             let func = func_table.index_for_func(func_key);
 
-            func_col.push(func);
+            func_col.push(func.0);
             let SubcategoryHandle(category, subcategory) = frame.subcategory;
             category_col.push(category);
             subcategory_col.push(subcategory);
@@ -92,7 +92,7 @@ impl FrameInterner {
 
             match frame.variant {
                 InternalFrameVariant::Label => {
-                    address_col.push(None);
+                    address_col.push(-1);
                     native_symbol_col.push(None);
                     inline_depth_col.push(0);
                 }
@@ -102,9 +102,9 @@ impl FrameInterner {
                     inline_depth,
                     ..
                 }) => {
-                    address_col.push(Some(relative_address));
+                    address_col.push(relative_address as i32);
                     native_symbol_col.push(native_symbol);
-                    inline_depth_col.push(inline_depth);
+                    inline_depth_col.push(inline_depth.min(u8::MAX as u16) as u8);
                 }
             }
         }
@@ -130,14 +130,14 @@ impl FrameInterner {
 }
 
 pub struct FrameTable {
-    func_col: Vec<FuncIndex>,
+    func_col: Vec<i32>,
     category_col: Vec<CategoryHandle>,
     subcategory_col: Vec<SubcategoryIndex>,
     line_col: Vec<Option<u32>>,
     column_col: Vec<Option<u32>>,
-    address_col: Vec<Option<u32>>,
+    address_col: Vec<i32>, // relative address, `-1` if None
     native_symbol_col: Vec<Option<NativeSymbolIndex>>,
-    inline_depth_col: Vec<u16>,
+    inline_depth_col: Vec<u8>,
 }
 
 impl FrameTable {
@@ -147,12 +147,7 @@ impl FrameTable {
             w.name("length")?;
             w.number_value(len)?;
             w.name("func")?;
-            w.array(|w| {
-                for f in &self.func_col {
-                    f.write_json(w)?;
-                }
-                Ok(())
-            })?;
+            w.number_array(&self.func_col)?;
             w.name("category")?;
             w.array(|w| {
                 for c in &self.category_col {
@@ -172,15 +167,7 @@ impl FrameTable {
             w.name("column")?;
             w.optional_number_array(&self.column_col)?;
             w.name("address")?;
-            w.array(|w| {
-                for a in &self.address_col {
-                    match a {
-                        Some(a) => w.number_value(*a)?,
-                        None => w.number_value(-1)?,
-                    }
-                }
-                Ok(())
-            })?;
+            w.number_array(&self.address_col)?;
             w.name("nativeSymbol")?;
             w.array(|w| {
                 for n in &self.native_symbol_col {
